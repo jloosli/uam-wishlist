0f112f7d721032327b089fdde61c68368f4e4de0
Jared Loosli <jloosli@gmail.com>
2014-10-30 13:43:29 -0600

    Initial Commit

---
+++ includes/class-uam-wishlist.php
@@ -0,0 +1,421 @@
+<?php
+
+if ( ! defined( 'ABSPATH' ) ) exit;
+
+class UAM_Wishlist {
+
+	/**
+	 * The single instance of UAM_Wishlist.
+	 * @var 	object
+	 * @access  private
+	 * @since 	1.0.0
+	 */
+	private static $_instance = null;
+
+	/**
+	 * Settings class object
+	 * @var     object
+	 * @access  public
+	 * @since   1.0.0
+	 */
+	public $settings = null;
+
+	/**
+	 * The version number.
+	 * @var     string
+	 * @access  public
+	 * @since   1.0.0
+	 */
+	public $_version;
+
+	/**
+	 * The token.
+	 * @var     string
+	 * @access  public
+	 * @since   1.0.0
+	 */
+	public $_token;
+
+	/**
+	 * The main plugin file.
+	 * @var     string
+	 * @access  public
+	 * @since   1.0.0
+	 */
+	public $file;
+
+	/**
+	 * The main plugin directory.
+	 * @var     string
+	 * @access  public
+	 * @since   1.0.0
+	 */
+	public $dir;
+
+	/**
+	 * The plugin assets directory.
+	 * @var     string
+	 * @access  public
+	 * @since   1.0.0
+	 */
+	public $assets_dir;
+
+	/**
+	 * The plugin assets URL.
+	 * @var     string
+	 * @access  public
+	 * @since   1.0.0
+	 */
+	public $assets_url;
+
+	/**
+	 * Suffix for Javascripts.
+	 * @var     string
+	 * @access  public
+	 * @since   1.0.0
+	 */
+	public $script_suffix;
+
+	public $object_types;
+
+	/**
+	 * Constructor function.
+	 * @access  public
+	 * @since   1.0.0
+	 * @return  void
+	 */
+	public function __construct ( $file = '', $version = '1.0.0' ) {
+		$this->_version = $version;
+		$this->_token = 'uAM_Wishlist';
+
+		// Load plugin environment variables
+		$this->file = $file;
+		$this->dir = dirname( $this->file );
+		$this->assets_dir = trailingslashit( $this->dir ) . 'assets';
+		$this->assets_url = esc_url( trailingslashit( plugins_url( '/assets/', $this->file ) ) );
+
+		$this->script_suffix = defined( 'SCRIPT_DEBUG' ) && SCRIPT_DEBUG ? '' : '.min';
+
+		register_activation_hook( $this->file, array( $this, 'install' ) );
+
+		// Load frontend JS & CSS
+		add_action( 'wp_enqueue_scripts', array( $this, 'enqueue_styles' ), 10 );
+		add_action( 'wp_enqueue_scripts', array( $this, 'enqueue_scripts' ), 10 );
+
+		// Load admin JS & CSS
+		add_action( 'admin_enqueue_scripts', array( $this, 'admin_enqueue_scripts' ), 10, 1 );
+		add_action( 'admin_enqueue_scripts', array( $this, 'admin_enqueue_styles' ), 10, 1 );
+
+		// Load API for generic admin functions
+		if ( is_admin() ) {
+			$this->admin = new UAM_Wishlist_Admin_API();
+		}
+
+		// Handle localisation
+		$this->load_plugin_textdomain();
+		add_action( 'init', array( $this, 'load_localisation' ), 0 );
+		if ( isset( $_GET['runit'] ) ) {
+			add_action( 'init', array($this,'run') );
+		}
+
+		$this->object_types = [ 'attachment', 'category', 'forum', 'page', 'post', 'role', 'user' ];
+	} // End __construct ()
+
+	public function run() {
+		echo "<pre>\n";
+//		echo "Members:\n";
+//		print_r($this->getMembers());
+		echo "Deleting Levels\n";
+		$this->deleteLevels();
+		echo "Creating groups\n";
+		$groups = $this->createGroups();
+		echo "Matching groups\n";
+		$this->matchGroups($groups);
+		echo "Completed\n";
+		ob_end_flush();
+		die;
+	}
+
+	function matchGroups($groups) {
+
+		echo "The New Groups:\n";
+		print_r($groups);
+		foreach ( $groups as $g ) {
+
+			$wishlist_id = $g['wishlist']['level']['id'];
+			echo "Adding wishlist id $wishlist_id\n";
+			$pages       = array_keys( $this->getObjects( 'page', $g['uam']->ID ) );
+			$posts       = array_keys( $this->getObjects( 'post', $g['uam']->ID ) );
+			$users       = array_keys( $this->getObjects( 'user', $g['uam']->ID ) );
+			$categories  = array_keys( $this->getObjects( 'category', $g['uam']->ID ) );
+			$this->addPosts( $wishlist_id, $pages );
+			$this->protect('page',$pages);
+			$this->addPosts( $wishlist_id, $posts );
+			$this->protect('post',$posts);
+			$this->addUsers( $wishlist_id, $users );
+			$this->addCategories( $wishlist_id, $categories );
+			$this->protect('category',$categories);
+			error_log("Adding wishlist id $wishlist_id\n");
+		}
+	}
+
+	function getMembers() {
+		$members = wlmapi_get_members();
+		return $members;
+	}
+
+
+	function deleteLevels() {
+		$levels = wlmapi_the_levels();
+		foreach ( $levels['levels']['level'] as $level ) {
+			echo "Level:\n";
+			print_r( $level );
+			echo "Members:\n";
+			$members = wlmapi_the_level_members($level['id']);
+			print_r($members);
+			foreach($members['members']['member'] as $member) {
+				echo "Removing {$member["user_login"]}\n";
+				wlmapi_remove_member_from_level( $level['id'], $member['id'] );
+			}
+			echo "Deleting level id: {$level['id']}\n";
+			$delete = wlmapi_delete_level( $level['id'] );
+			if(!$delete['success']) {
+				print_r($delete);
+				die;
+			}
+		}
+	}
+
+	function addUsers( $group_id, $user_ids ) {
+		$args    = array( 'Users' => $user_ids );
+		$members = wlmapi_add_member_to_level( $group_id, $args );
+		echo $group_id . "\n";
+		print_r( $members );
+
+		return $members;
+	}
+
+
+	// Returns all access groups from User Access Manager
+	function getAccessgroups() {
+		global $wpdb;
+		$sql = "SELECT ID, groupname FROM {$wpdb->prefix}uam_accessgroups";
+
+		return $wpdb->get_results( $sql, OBJECT_K );
+	}
+
+	// Create Groups
+	function createGroup( $args ) {
+		$level = wlmapi_create_level( $args );
+		print_r( $level );
+
+		return $level;
+	}
+
+	function createGroups() {
+		echo "Creating Groups";
+		$groups = [ ];
+
+		foreach ( $this->getAccessgroups() as $grp ) {
+			$args     = [ 'name' => $grp->groupname ];
+			$g        = $this->createGroup( $args );
+			$groups[] = [ 'uam' => $grp, 'wishlist' => $g ];
+		}
+		return $groups;
+	}
+
+	function getObjects( $type, $group_id ) {
+		echo "Getting $type, group id: $group_id\n";
+		global $wpdb;
+		$sql = "SELECT object_id, group_id FROM {$wpdb->prefix}uam_accessgroup_to_object WHERE object_type = '$type' AND group_id = $group_id";
+
+		return $wpdb->get_results( $sql, OBJECT_K );
+	}
+
+	function addPosts( $level_id, $post_ids = [ ] ) {
+		$posts = wlmapi_manage_post( $level_id, $post_ids );
+
+		return $posts;
+	}
+
+	/**
+	 * @param string $type Content type: category|page|post
+	 * @param array  $ids
+	 *
+	 * @return array
+	 */
+	function protect($type, $ids = []) {
+		$args = array('ContentIds' => $ids);
+		$func = "wlmapi_protect_$type";
+		return $func($args);
+	}
+
+	function addCategories( $level_id, $category_ids = [ ] ) {
+		$args       = array( 'ContentIds' => $category_ids );
+		$categories = wlmapi_protect_category( $args );
+
+		return $categories;
+	}
+
+
+
+
+	/**
+	 * Wrapper function to register a new post type
+	 * @param  string $post_type   Post type name
+	 * @param  string $plural      Post type item plural name
+	 * @param  string $single      Post type item single name
+	 * @param  string $description Description of post type
+	 * @return object              Post type class object
+	 */
+	public function register_post_type ( $post_type = '', $plural = '', $single = '', $description = '' ) {
+
+		if ( ! $post_type || ! $plural || ! $single ) return;
+
+		$post_type = new UAM_Wishlist_Post_Type( $post_type, $plural, $single, $description );
+
+		return $post_type;
+	}
+
+	/**
+	 * Wrapper function to register a new taxonomy
+	 * @param  string $taxonomy   Taxonomy name
+	 * @param  string $plural     Taxonomy single name
+	 * @param  string $single     Taxonomy plural name
+	 * @param  array  $post_types Post types to which this taxonomy applies
+	 * @return object             Taxonomy class object
+	 */
+	public function register_taxonomy ( $taxonomy = '', $plural = '', $single = '', $post_types = array() ) {
+
+		if ( ! $taxonomy || ! $plural || ! $single ) return;
+
+		$taxonomy = new UAM_Wishlist_Taxonomy( $taxonomy, $plural, $single, $post_types );
+
+		return $taxonomy;
+	}
+
+	/**
+	 * Load frontend CSS.
+	 * @access  public
+	 * @since   1.0.0
+	 * @return void
+	 */
+	public function enqueue_styles () {
+		wp_register_style( $this->_token . '-frontend', esc_url( $this->assets_url ) . 'css/frontend.css', array(), $this->_version );
+		wp_enqueue_style( $this->_token . '-frontend' );
+	} // End enqueue_styles ()
+
+	/**
+	 * Load frontend Javascript.
+	 * @access  public
+	 * @since   1.0.0
+	 * @return  void
+	 */
+	public function enqueue_scripts () {
+		wp_register_script( $this->_token . '-frontend', esc_url( $this->assets_url ) . 'js/frontend' . $this->script_suffix . '.js', array( 'jquery' ), $this->_version );
+		wp_enqueue_script( $this->_token . '-frontend' );
+	} // End enqueue_scripts ()
+
+	/**
+	 * Load admin CSS.
+	 * @access  public
+	 * @since   1.0.0
+	 * @return  void
+	 */
+	public function admin_enqueue_styles ( $hook = '' ) {
+		wp_register_style( $this->_token . '-admin', esc_url( $this->assets_url ) . 'css/admin.css', array(), $this->_version );
+		wp_enqueue_style( $this->_token . '-admin' );
+	} // End admin_enqueue_styles ()
+
+	/**
+	 * Load admin Javascript.
+	 * @access  public
+	 * @since   1.0.0
+	 * @return  void
+	 */
+	public function admin_enqueue_scripts ( $hook = '' ) {
+		wp_register_script( $this->_token . '-admin', esc_url( $this->assets_url ) . 'js/admin' . $this->script_suffix . '.js', array( 'jquery' ), $this->_version );
+		wp_enqueue_script( $this->_token . '-admin' );
+	} // End admin_enqueue_scripts ()
+
+	/**
+	 * Load plugin localisation
+	 * @access  public
+	 * @since   1.0.0
+	 * @return  void
+	 */
+	public function load_localisation () {
+		load_plugin_textdomain( 'uam-wishlist', false, dirname( plugin_basename( $this->file ) ) . '/lang/' );
+	} // End load_localisation ()
+
+	/**
+	 * Load plugin textdomain
+	 * @access  public
+	 * @since   1.0.0
+	 * @return  void
+	 */
+	public function load_plugin_textdomain () {
+	    $domain = 'uam-wishlist';
+
+	    $locale = apply_filters( 'plugin_locale', get_locale(), $domain );
+
+	    load_textdomain( $domain, WP_LANG_DIR . '/' . $domain . '/' . $domain . '-' . $locale . '.mo' );
+	    load_plugin_textdomain( $domain, false, dirname( plugin_basename( $this->file ) ) . '/lang/' );
+	} // End load_plugin_textdomain ()
+
+	/**
+	 * Main UAM_Wishlist Instance
+	 *
+	 * Ensures only one instance of UAM_Wishlist is loaded or can be loaded.
+	 *
+	 * @since 1.0.0
+	 * @static
+	 * @see UAM_Wishlist()
+	 * @return Main UAM_Wishlist instance
+	 */
+	public static function instance ( $file = '', $version = '1.0.0' ) {
+		if ( is_null( self::$_instance ) ) {
+			self::$_instance = new self( $file, $version );
+		}
+		return self::$_instance;
+	} // End instance ()
+
+	/**
+	 * Cloning is forbidden.
+	 *
+	 * @since 1.0.0
+	 */
+	public function __clone () {
+		_doing_it_wrong( __FUNCTION__, __( 'Cheatin&#8217; huh?' ), $this->_version );
+	} // End __clone ()
+
+	/**
+	 * Unserializing instances of this class is forbidden.
+	 *
+	 * @since 1.0.0
+	 */
+	public function __wakeup () {
+		_doing_it_wrong( __FUNCTION__, __( 'Cheatin&#8217; huh?' ), $this->_version );
+	} // End __wakeup ()
+
+	/**
+	 * Installation. Runs on activation.
+	 * @access  public
+	 * @since   1.0.0
+	 * @return  void
+	 */
+	public function install () {
+		$this->_log_version_number();
+	} // End install ()
+
+	/**
+	 * Log the plugin version number.
+	 * @access  public
+	 * @since   1.0.0
+	 * @return  void
+	 */
+	private function _log_version_number () {
+		update_option( $this->_token . '_version', $this->_version );
+	} // End _log_version_number ()
+
+}
---
+++ uam-wishlist.php
@@ -0,0 +1,47 @@
+<?php
+/*
+ * Plugin Name: User Access Manager to Wishlist Plugin
+ * Version: 1.0
+ * Plugin URI: http://www.hughlashbrooke.com/
+ * Description: This is your starter template for your next WordPress plugin.
+ * Author: Hugh Lashbrooke
+ * Author URI: http://www.hughlashbrooke.com/
+ * Requires at least: 4.0
+ * Tested up to: 4.0
+ *
+ * Text Domain: uam-wishlist
+ * Domain Path: /lang/
+ *
+ * @package WordPress
+ * @author Hugh Lashbrooke
+ * @since 1.0.0
+ */
+
+if ( ! defined( 'ABSPATH' ) ) exit;
+
+// Load plugin class files
+require_once( 'includes/class-uam-wishlist.php' );
+require_once( 'includes/class-uam-wishlist-settings.php' );
+
+// Load plugin libraries
+require_once( 'includes/lib/class-uam-wishlist-admin-api.php' );
+require_once( 'includes/lib/class-uam-wishlist-post-type.php' );
+require_once( 'includes/lib/class-uam-wishlist-taxonomy.php' );
+
+/**
+ * Returns the main instance of UAM_Wishlist to prevent the need to use globals.
+ *
+ * @since  1.0.0
+ * @return object UAM_Wishlist
+ */
+function UAM_Wishlist () {
+	$instance = UAM_Wishlist::instance( __FILE__, '1.0.0' );
+
+	if ( is_null( $instance->settings ) ) {
+		$instance->settings = UAM_Wishlist_Settings::instance( $instance );
+	}
+
+	return $instance;
+}
+
+UAM_Wishlist();
